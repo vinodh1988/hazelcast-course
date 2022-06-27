fc3ce3c2a50a49c9dd223556db562a9abe57e8dd
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-27 12:11:25 +0530

    hazelcast-cluster

--- hazelcast-app-server/src/main/java/com/app/HazelcastServer.java
+++ hazelcast-app-server/src/main/java/com/app/HazelcastServer.java
@@ -1,10 +1,16 @@
 package com.app;
 
+import java.util.Map;
+
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 
 public class HazelcastServer {
     public static void main(String n[]) {
     	HazelcastInstance instance=Hazelcast.newHazelcastInstance();
+    	Map<Integer,String> map=instance.getMap("people");
+    	map.put(1, "Raju");
+    	map.put(2, "Ravi");
+    	map.put(3, "Harish");
     }
 }
