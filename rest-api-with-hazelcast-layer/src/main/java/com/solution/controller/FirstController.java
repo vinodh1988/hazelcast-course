cc6dfe0ef49a8bff0e7a8e2a42a8c85b95447610
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-28 12:07:39 +0530

    topic-commit

--- rest-api-with-hazelcast-layer/src/main/java/com/solution/controller/FirstController.java
+++ rest-api-with-hazelcast-layer/src/main/java/com/solution/controller/FirstController.java
@@ -1,20 +1,31 @@
 package com.solution.controller;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import javax.print.attribute.standard.DateTimeAtCompleted;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hazelcast.topic.ITopic;
 
 @RestController
 @RequestMapping("/api")
 public class FirstController {
+	
+	@Autowired
+	ITopic<String> topic;
   
 	@RequestMapping("/request")
 	public String greet( ) {
+		topic.publish("A request has been made @ "+LocalDateTime.now());
 		return "Hi!!!  a request made @ "+ LocalDateTime.now();
+	}
+	
+	@RequestMapping("/message/{message}")
+	public String getMessage(@PathVariable("message") String message) {
+		topic.publish("Message received ->"+message);
+		return "Message processed";
 	}
 	
 }
