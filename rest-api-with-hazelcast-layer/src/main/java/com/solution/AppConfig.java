ba0e5bb43d6de89514f5c91d2a16dc511558310a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-28 11:45:17 +0530

    hazelcast-config

--- rest-api-with-hazelcast-layer/src/main/java/com/solution/AppConfig.java
+++ rest-api-with-hazelcast-layer/src/main/java/com/solution/AppConfig.java
@@ -1,5 +1,21 @@
 package com.solution;
 
-public class AppConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.topic.ITopic;
+
+@Configuration
+public class AppConfig {
+	
+@Autowired
+HazelcastInstance instance;
+
+  @Bean
+  public ITopic<String> getTopic(){
+	  return instance.getTopic("restapp");
+  }
 }
--- rest-api-with-hazelcast-layer/src/main/java/com/solution/RestApiWithHazelcastLayerApplication.java
+++ rest-api-with-hazelcast-layer/src/main/java/com/solution/RestApiWithHazelcastLayerApplication.java
@@ -2,9 +2,19 @@ package com.solution;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 @SpringBootApplication
 public class RestApiWithHazelcastLayerApplication {
+
+	  @Bean
+	  public HazelcastInstance getInstance() {
+		  return Hazelcast.newHazelcastInstance();
+	  }
+	  
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiWithHazelcastLayerApplication.class, args);
