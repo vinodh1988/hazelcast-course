d6fe9dd201db9b4468da780c481940c421b0bdbd
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-29 08:50:09 +0530

    new-project

---
+++ rest-api-with-cache-layer/src/main/java/com/solution/RestApiWithCacheLayerApplication.java
@@ -0,0 +1,13 @@
+package com.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithCacheLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithCacheLayerApplication.class, args);
+	}
+
+}
